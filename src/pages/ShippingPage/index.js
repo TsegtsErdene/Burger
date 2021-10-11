3c6ae9f7f47a2c65e6138e9f3cf759bdf7cface2
tsegtsee <ttsegtsee@gmail.com>
2021-10-11 18:07:20 +0800

    hicheel 61

--- src/pages/ShippingPage/index.js
+++ src/pages/ShippingPage/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import { Route } from "react-router";
 import Burger from "../../components/Burger";
+import ContactData from "../../components/ContactData";
 import Button from "../../components/General/Button";
 import css from "./style.module.css";
 
 export class ShippingPagge extends Component {
   state = {
+    dun: 0,
+
     ingredients: {
       salad: 0,
       cheese: 0,
@@ -26,15 +30,28 @@ export class ShippingPagge extends Component {
   goBack = () => {
     this.props.history.goBack();
   };
+
+  showContactData = () => {
+    this.props.history.push("/ship/contact");
+  };
   render() {
     return (
-      <div className="ShippingPage">
+      <div className={css.ShippingPage}>
+        <p style={{ fontSize: "24px" }}>
+          <strong> Таны захиалга амттай байх болно.</strong>
+        </p>
         <Burger orts={this.state.ingredients} />
         <Button
           clicked={this.goBack}
           btnType="Danger"
-          text="Захиалгыг цуцлах"
+          text="ЗАХИАЛГЫГ ЦУЦЛАХ"
         />
+        <Button
+          clicked={this.showContactData}
+          btnType="Success"
+          text="ХҮРГЭЛТИЙН МЭДЭЭЛЭЛ ОРУУЛАХ "
+        />
+        <Route path="/ship/contact" component={ContactData} />
       </div>
     );
   }
